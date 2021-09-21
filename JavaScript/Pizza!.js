c5aec2b6684d99fa1ba44869e592554d04465012
SPARRYW <scott.westerbeck@gmail.com>
2021-09-21 16:12:55 -0400

    Pizza fail misunderstood redo

---
+++ JavaScript/Pizza!.js
@@ -0,0 +1,9 @@
+function pizzaOven (crust, sauce, cheese, toppings) {
+    var pizza = {};
+    pizza.crust = [deep dish, hand tossed]
+    pizza.sauce = [traditional, marinara, white]
+    pizza.cheese = [mozzarella, feta]
+    pizza.toppings = [pepperoni, sausage, mushrooms, olives, onions]
+    return pizza;
+}
+console.log(pizzaOven);
